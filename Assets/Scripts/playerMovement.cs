740478f00aeff88fd1c6eafb5d384fee00c781c2
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2022-10-16 18:36:24 -0400

    Organization

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,15 +7,18 @@ public class playerMovement : MonoBehaviour
     [SerializeField] private float horizontalSpeed = 10f;
     [SerializeField] private float VerticalSpeed = 0f;
     private Rigidbody2D rb;
+    private SpriteRenderer sp;
    
     [SerializeField] float movementSmooth = 0.5f;
     private Vector3 velocity = Vector3.zero;
     private bool canMove = true;
     public HealthBar healthBar;
+    private bool facingRight;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
     }
     public void Move(float horizMove, float vertMove)
     {
@@ -24,21 +27,23 @@ public class playerMovement : MonoBehaviour
             Vector3 targetVelocity = new Vector2(horizMove * horizontalSpeed, vertMove * VerticalSpeed);
             rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmooth);
 
-           /* if(horizMove > 0 && !facingRight)
+            if(horizMove < 0 && !facingRight)
             {
                 flip();
-            }else if(horizMove < 0 && facingRight)
+                sp.flipX = true;
+            }
+            else if(horizMove > 0 && facingRight)
             {
                 flip();
-            }*/
+                sp.flipX = false;
+            }
         }
     }
 
-    /*private void flip()
+    private void flip()
     {
         facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
-    } */
+    } 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
