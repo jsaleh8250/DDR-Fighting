5734450ab24514db9445b48c2944992cbb4f351f
jsaleh8250 <jsaleh@quinnipiac.edu>
2022-10-24 01:34:38 -0400

    simple restart button/MVP build

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    int SceneIndex;
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
