28c41b57522ace96bd941a52e3046ee874f8b9f3
jsaleh8250 <jsaleh@quinnipiac.edu>
2022-11-15 21:27:32 -0500

    Can use pause menu with controller

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Pause : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject PauseMenu;
+    public GameObject resumebutton, optionsbutton , optionsClosed, quitbutton;
 
     // Update is called once per frame
     void Update()
@@ -28,6 +30,8 @@ public class Pause : MonoBehaviour
         PauseMenu.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(resumebutton);
     }
 
     void Pause1()
