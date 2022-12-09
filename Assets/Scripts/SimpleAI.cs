b245f674aca0bb99f611e23971ec3f8772f704e0
Jenna Saleh <54547992+jsaleh8250@users.noreply.github.com>
2022-12-08 20:28:09 -0500

    Timebar change

--- Assets/Scripts/SimpleAI.cs
+++ Assets/Scripts/SimpleAI.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SimpleAI : MonoBehaviour
 {
     public Transform player;
+    public Transform other;
     public float speed;
     private float distance;
     public float stoppingDist;
+    //public float enemystoppingDist;
 
     //Anim
     Animator enemyAnim;
@@ -17,11 +19,14 @@ public class SimpleAI : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         enemyAnim = GetComponent<Animator>();
+        //other = GameObject.FindGameObjectWithTag("BattleSeq").transform;
+        //other = GameObject.Find("Simple AI(1)").transform;
     }
 
     void Update()
     {
         EnemyMovement();
+       // other = GameObject.Find("Simple AI").transform;
     }
 
     void EnemyMovement()
@@ -30,20 +35,27 @@ public class SimpleAI : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
+       
+        //if (Vector2.Distance(transform.position, other.position) > enemystoppingDist)
+       // { 
+            //If at a distance from player it will stop moving towards player
+            if (Vector2.Distance(transform.position, player.position) > stoppingDist)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.position.x, transform.position.y), speed * Time.deltaTime);
+                ChangingAnim("Walking");
 
-        //If at a distance from player it will stop moving towards player
-        if (Vector2.Distance(transform.position, player.position) > stoppingDist)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.position.x, transform.position.y), speed * Time.deltaTime);
-            ChangingAnim("Walking");
+            }
+            else
+            {
+                ChangingAnim("Idle");
+            }
 
-        }
-        else
-        {
+       // }
+       //else {
             ChangingAnim("Idle");
-        }
-
+       // }
     }
+
 
     void ChangingAnim(string newState)
     {
@@ -51,5 +63,5 @@ public class SimpleAI : MonoBehaviour
         enemyAnim.Play(newState);
         currentState = newState;
     }
-
+   
 }
