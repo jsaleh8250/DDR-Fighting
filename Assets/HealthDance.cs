45409495e113898a031c7a01f8bc8f8713021928
Mario-Egocheaga <work.mario.egocheaga@gmail.com>
2023-03-28 23:09:32 -0400

    Health Cam

---
+++ Assets/HealthDance.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDance : MonoBehaviour
+{
+    public float songTime;
+    public GameObject HealthCam;
+    void Start()
+    {
+        HealthCam = GameObject.FindGameObjectWithTag("HealthCam");
+        StartCoroutine(LoadNextLevel(songTime));
+    }
+
+
+    IEnumerator LoadNextLevel(float songTIme)
+    {
+        yield return new WaitForSeconds(songTime);
+        GameManager.inHealthMode = false;
+        HealthCam.SetActive(false);
+    }
+}
